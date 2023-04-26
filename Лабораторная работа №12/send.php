1e6870c02c615686f96b032ae12114d0a6f339c2
Bobrov Vladislav <bobroff.vladisl@yandex.ru>
2023-04-26 15:11:22 +0300

    Лабораторная работа №12

---
+++ Лабораторная работа №12/send.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' && !empty($_POST['name'])) {
+    $message = 'Имя: ' . $_POST['name'] . ' <br> ';
+    $message .= 'Фамилия: ' . $_POST['second_name'] . ' <br> ';
+    $message .= 'Телефон: ' . $_POST['phone'] . ' <br> ';
+    $message .= 'Email: ' . $_POST['email'] . ' <br> ';
+    $message .= 'Компания: ' . $_POST['company'] . ' <br> ';
+    $message .= 'Как мы можем вам помочь: ' . $_POST['coment'] . ' <br> ';
+
+    $mailTo = "borovsky.yan2017@yandex.ru"; // Ваш e-mail
+    $subject = "Письмо с сайта"; // Тема сообщения
+    $headers= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8\r\n";
+    $headers .= "From: info@site.ru <info@site.ru>\r\n";
+    if(mail($mailTo, $subject, $message, $headers)) {
+        echo "Сообщение отправлено!"; 
+    } else {
+        echo "Сообщение не отправлено!"; 
+    }
+}
+?>
